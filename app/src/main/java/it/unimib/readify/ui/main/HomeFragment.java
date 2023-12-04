842705d865d81af48d4f97142ff66005ceb206e0
Mattia Ingrassia <tiaingra@gmail.com>
2023-12-04 10:42:16 +0100

    edited fragment home for testing purposes

--- app/src/main/java/it/unimib/readify/ui/main/HomeFragment.java
+++ app/src/main/java/it/unimib/readify/ui/main/HomeFragment.java
@@ -1,16 +1,23 @@
 package it.unimib.readify.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import it.unimib.readify.R;
+import it.unimib.readify.ui.startup.WelcomeActivity;
 
 public class HomeFragment extends Fragment {
+
+    private static final boolean USE_NAVIGATION_COMPONENT = true;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -29,6 +36,27 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        final Button buttonDaRimuovere = rootView.findViewById(R.id.button_da_rimuovere);
+
+        buttonDaRimuovere.setOnClickListener(v -> {
+            navigateToHomeActivity();
+        });
+
+        return rootView;
     }
+
+
+
+
+    private void navigateToHomeActivity() {
+        if (USE_NAVIGATION_COMPONENT) {
+            Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment_to_bookDetailsFragment);
+        } else {
+            Intent intent = new Intent(requireContext(), HomeActivity.class);
+            startActivity(intent);
+            requireActivity().finish();
+        }
+    }
+
 }

851aa7e807113d6a971dae178ca00525d80580ba
Mattia-LT <tiaingra@gmail.com>
2023-12-04 13:38:30 +0100

    fixed SearchFragment problem with filters

--- app/src/main/java/it/unimib/readify/ui/main/FilterBottomSheet.java
+++ app/src/main/java/it/unimib/readify/ui/main/FilterBottomSheet.java
@@ -8,14 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import it.unimib.readify.R;
 
 public class FilterBottomSheet extends BottomSheetDialogFragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.bottom_search_filter, container, false);
 
     }
 
@@ -23,6 +26,11 @@ public class FilterBottomSheet extends BottomSheetDialogFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        // Get the BottomSheetBehavior
+        BottomSheetBehavior<View> behavior = BottomSheetBehavior.from((View) view.getParent());
+
+        // Set the state to EXPANDED
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
     }
 
@@ -30,6 +38,7 @@ public class FilterBottomSheet extends BottomSheetDialogFragment {
     public void onDestroyView() {
         super.onDestroyView();
     }
+
 
 
 
--- app/src/main/java/it/unimib/readify/ui/main/SearchFragment.java
+++ app/src/main/java/it/unimib/readify/ui/main/SearchFragment.java
@@ -2,11 +2,16 @@ package it.unimib.readify.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.button.MaterialButton;
 
 import it.unimib.readify.R;
 
@@ -26,10 +31,21 @@ public class SearchFragment extends Fragment {
 
     }
 
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_search, container, false);
+
+        MaterialButton filterButton = rootview.findViewById(R.id.button_search_filter);
+
+        FilterBottomSheet filterBottomSheet = new FilterBottomSheet();
+
+        filterButton.setOnClickListener( view -> {
+            filterBottomSheet.show(getChildFragmentManager(), filterBottomSheet.getTag());
+        });
+
+        return rootview;
     }
 }

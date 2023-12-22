015819a6c2f7eb3f90f5daa1a56e7701832a000e
Mattia-LT <tiaingra@gmail.com>
2023-12-22 12:53:17 +0100

    created classes for future database implementation

---
+++ app/src/main/java/it/unimib/readify/database/BookDao.java
@@ -0,0 +1,4 @@
+package it.unimib.readify.database;
+
+public interface BookDao {
+}
---
+++ app/src/main/java/it/unimib/readify/database/BookRoomDatabase.java
@@ -0,0 +1,17 @@
+package it.unimib.readify.database;
+
+import android.content.Context;
+
+import androidx.room.RoomDatabase;
+
+public abstract class BookRoomDatabase extends RoomDatabase {
+    //todo implementa metodi, questa è una bozza per non avere errori
+    private static volatile BookRoomDatabase INSTANCE;
+    public abstract BookDao bookDao();
+
+
+    public static BookRoomDatabase getDatabase(final Context context) {
+
+        return INSTANCE;
+    }
+}

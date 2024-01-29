0c155d3e21337d9ac48383946795321e7dde0557
emamo <ema.mordy@gmail.com>
2024-01-29 16:49:55 +0100

    deleted unnecessary methods in OLWorkApiResponse added bindings in ProfileFragment

--- app/src/main/java/it/unimib/readify/model/OLWorkApiResponse.java
+++ app/src/main/java/it/unimib/readify/model/OLWorkApiResponse.java
@@ -13,15 +13,12 @@ public class OLWorkApiResponse implements Parcelable {
 
     private OLDescription description;
     private String title;
-
     private List<Integer> covers;
-
     @SerializedName("first_publish_date")
     private String firstPublishDate;
     private String key;
     private List<OLAuthorKeys> authors;
     private List<String> subjects;
-
     private List<OLAuthorApiResponse> authorList;
 
     public OLWorkApiResponse() {}
@@ -58,16 +55,6 @@ public class OLWorkApiResponse implements Parcelable {
 
     public List<Integer> getCovers() {
         return covers;
-    }
-
-    public int getCover(int position) {
-        return covers.get(position);
-    }
-
-    public int getNumberOfCovers() {
-        if(covers == null)
-            return 0;
-        return covers.size();
     }
 
     public void setCovers(List<Integer> covers) {

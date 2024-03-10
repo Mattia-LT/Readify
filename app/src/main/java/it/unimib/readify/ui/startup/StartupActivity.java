8e0b307f174b377ee3b4b392e5611adf23a41649
Mattia-LT <tiaingra@gmail.com>
2024-03-11 00:24:38 +0100

    Update StartupActivity

--- app/src/main/java/it/unimib/readify/ui/startup/StartupActivity.java
+++ app/src/main/java/it/unimib/readify/ui/startup/StartupActivity.java
@@ -2,34 +2,26 @@ package it.unimib.readify.ui.startup;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import it.unimib.readify.R;
+import it.unimib.readify.databinding.ActivityStartupBinding;
 
 public class StartupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_startup);
-
-        Button btn_getting_started = findViewById(R.id.button_startup);
-
-        btn_getting_started.setOnClickListener(view -> {
+        ActivityStartupBinding binding = ActivityStartupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //TODO controllo login -> avanzo skippando questa parte
+        binding.buttonStartup.setOnClickListener(view -> {
             Intent intent = new Intent(this, WelcomeActivity.class);
-            // intent.putExtra(key, value);
-
-
-
-            // Dobbiamo usare i bundles, lo sistemo appena recupero le esercitazioni
-            // Bundle bundle = new Bundle();
-            // intent.putExtras(bundle);
-
-
             startActivity(intent);
-            //finish();
+            finish();
         });
 
 

cf93b2983f5c3fce5c1e2f869af9f7c856edf7cf
Mattia-LT <tiaingra@gmail.com>
2024-03-11 00:25:07 +0100

    Update HomeActivity

--- app/src/main/java/it/unimib/readify/ui/main/HomeActivity.java
+++ app/src/main/java/it/unimib/readify/ui/main/HomeActivity.java
@@ -15,12 +15,11 @@ import it.unimib.readify.R;
 import it.unimib.readify.databinding.ActivityHomeBinding;
 public class HomeActivity extends AppCompatActivity {
 
-    private ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         MaterialToolbar toolbar = binding.topAppbarHome;

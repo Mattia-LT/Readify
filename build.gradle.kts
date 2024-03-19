7cd38c3d73aab38d03b2af70c52088093135dcf7
Mattia-LT <tiaingra@gmail.com>
2024-03-19 15:46:31 +0100

    update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
     // Dependency for google services
     id("com.google.gms.google-services") version "4.4.0" apply false
 }

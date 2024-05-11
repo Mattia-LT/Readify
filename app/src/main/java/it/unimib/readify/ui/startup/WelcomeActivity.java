1620ec1844543b82ffbe09a02079886a901911cd
Mattia-LT <tiaingra@gmail.com>
2024-05-11 19:47:28 +0200

    Cleaned up some code

--- app/src/main/java/it/unimib/readify/ui/startup/WelcomeActivity.java
+++ app/src/main/java/it/unimib/readify/ui/startup/WelcomeActivity.java
@@ -7,15 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import it.unimib.readify.R;
 
 
-// TODO gestire il login / registrazione.
 public class WelcomeActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-
     }
-
-
 }
